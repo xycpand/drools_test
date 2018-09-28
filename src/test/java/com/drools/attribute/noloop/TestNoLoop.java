41a246321a7069ae28dafddc01ade96242427076
lcc <541711153@qq.com>
2018-09-28 16:13:21 +0800

    no-loop 属性 的测试类

--- src/test/java/com/drools/attribute/noloop/TestNoLoop.java
+++ src/test/java/com/drools/attribute/noloop/TestNoLoop.java
@@ -1,4 +1,4 @@
-package com.test;
+package com.drools.attribute.noloop;
 
 import com.secbro.drools.model.Product;
 import org.junit.Test;
@@ -7,17 +7,21 @@ import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
 /**
- * Created by lcc on 2018/9/27.
+ * Created by lcc on 2018/9/28.
  */
-public class TestRules {
+public class TestNoLoop {
 
+
+    /**
+     * 测试博客上的 no-loop 属性 true
+     */
     @Test
-    public void testRules() {
+    public void onLoopTest1() {
         // 构建KieServices
         KieServices ks = KieServices.Factory.get();
         KieContainer kieContainer = ks.getKieClasspathContainer();
         // 获取kmodule.xml中配置中名称为ksession-rule的session，默认为有状态的。
-        KieSession kSession = kieContainer.newKieSession("ksession-rule");
+        KieSession kSession = kieContainer.newKieSession("noLoopSession");
 
         Product product = new Product();
         product.setType(Product.GOLD);
@@ -30,26 +34,26 @@ public class TestRules {
 
         kSession.dispose();
 
-
     }
 
 
     /**
-     * 测试博客上的 no-loop 属性
+     * 测试博客上的 no-loop 属性 fale
      */
     @Test
-    public void testRules1() {
+    public void onLoopTest2() {
         // 构建KieServices
         KieServices ks = KieServices.Factory.get();
         KieContainer kieContainer = ks.getKieClasspathContainer();
         // 获取kmodule.xml中配置中名称为ksession-rule的session，默认为有状态的。
-        KieSession kSession = kieContainer.newKieSession("noLoopSession");
+        KieSession kSession = kieContainer.newKieSession("noLoopSession2");
 
         Product product = new Product();
         product.setType(Product.GOLD);
-        product.setDiscount(10);
+        product.setDiscount(12);
 
         kSession.insert(product);
+
         int count = kSession.fireAllRules();
         System.out.println("命中了" + count + "条规则！");
         System.out.println("商品" +product.getType() + "的商品折扣为" + product.getDiscount() + "%。");
